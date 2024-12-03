900f27d0a652542c15d7dd9052fd6102c7c99fb9
MURILO CAROLINO DOS SANTOS <23111821@CFP127ALUNO.LOCAL>
2024-12-03 12:22:25 -0300

    doaacao

--- src/pages/ChatList.jsx
+++ src/pages/ChatList.jsx
@@ -1,3 +1,102 @@
+// import React, { useState, useEffect } from 'react';
+// import { getDatabase, ref, push, onValue } from 'firebase/database';
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBP1Spabyka90r29lNGavIZUbG0CmGf7ik",
+//     authDomain: "callme-chat-9277c.firebaseapp.com",
+//     databaseURL: "https://callme-chat-9277c-default-rtdb.firebaseio.com",
+//     projectId: "callme-chat-9277c",
+//     storageBucket: "callme-chat-9277c.firebasestorage.app",
+//     messagingSenderId: "948644736433",
+//     appId: "1:948644736433:web:ada741a16a9bbbb8e1e190"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+
+// const Chat = ({ chatId }) => {
+//     const [novaMensagem, setNovaMensagem] = useState('');
+//     const [mensagens, setMensagens] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//     const [usuario, setUsuario] = useState({ nome: '', foto: '' });
+    
+//     const id_cliente = localStorage.getItem('id_cliente');
+
+//     // Verifique se id_cliente e chatId estão definidos
+//     if (!id_cliente || !chatId) {
+//         return <div>Erro: ID do cliente ou Chat ID não estão definidos.</div>;
+//     }
+
+//     const chatIdUnico = `C${id_cliente}_U${chatId}`;
+
+//     useEffect(() => {
+//         const chatRef = ref(database, `chats/${chatIdUnico}/conversa`);
+//         const mensagensListener = onValue(chatRef, (snapshot) => {
+//             if (snapshot.exists()) {
+//                 const data = snapshot.val();
+//                 const mensagensArray = Object.values(data);
+//                 const mensagensFiltradas = mensagensArray.filter(msg =>
+//                     msg.id_cliente === id_cliente || msg.id_usuario === chatId
+//                 );
+//                 setMensagens(mensagensFiltradas);
+//             }
+//             setLoading(false);
+//         });
+
+//         return () => {
+//             mensagensListener();
+//         };
+//     }, [chatIdUnico, id_cliente, chatId]);
+
+//     const enviarMensagem = () => {
+//         if (novaMensagem.trim() === '') return; // Verifique se a mensagem não está vazia
+
+//         const mensagem = {
+//             id_cliente,
+//             id_usuario: chatId,
+//             texto: novaMensagem,
+//             timestamp: Date.now(),
+//         };
+
+//         const chatRef = ref(database, `chats/${chatIdUnico}/conversa`);
+//         push(chatRef, mensagem)
+//             .then(() => {
+//                 setNovaMensagem(''); // Limpa o campo de entrada após o envio
+//             })
+//             .catch((error) => {
+//                 console.error('Erro ao enviar mensagem:', error);
+//             });
+//     };
+
+//     if (loading) {
+//         return <div>Carregando mensagens...</div>;
+//     }
+
+//     return (
+//         <div>
+//             <h2>Chat com {usuario.nome}</h2>
+//             <img src={usuario.foto} alt={usuario.nome} />
+//             <div>
+//                 {mensagens.map((msg, index) => (
+//                     <div key={index} className={msg.id_cliente === id_cliente ? 'minha-mensagem' : 'mensagem-recebida'}>
+//                         <p>{msg.texto}</p>
+//                     </div>
+//                 ))}
+//             </div>
+//             <input
+//                 type="text"
+//                 value={novaMensagem}
+//                 onChange={(e) => setNovaMensagem(e.target.value)}
+//                 placeholder="Digite sua mensagem..."
+//             />
+//             <button onClick={enviarMensagem}>Enviar</button>
+//         </div>
+//     );
+// };
+
+// export default Chat;
+
 import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
--- src/pages/Doacao.jsx
+++ src/pages/Doacao.jsx
@@ -6,7 +6,7 @@ import styles from '../css/Doacao.module.css';
 const Doacao = () => {
     const [selectedAmount, setSelectedAmount] = useState(0);
     const [selectedButton, setSelectedButton] = useState(null);
-    const [currentPage, setCurrentPage] = useState("home"); // Estado para alternar entre páginas
+    const [currentPage, setCurrentPage] = useState("home");
 
     const handleButtonClick = (amount, buttonType) => {
         setSelectedAmount(amount);
@@ -20,119 +20,100 @@ const Doacao = () => {
     };
 
     const getProgressPercentage = () => {
-        if (selectedAmount >= 20) return 100;
-        return (selectedAmount / 20) * 100;
+        if (selectedAmount >= 30) return 100;
+        return (selectedAmount / 30) * 100;
     };
 
-    const handlePixClick = () => {
-        setCurrentPage("pix"); // Muda para a página PIX
-    };
-
-    const handleBackClick = () => {
-        setCurrentPage("home"); // Volta para a página inicial
-    };
+    const handlePixClick = () => setCurrentPage("pix");
+    const handleBackClick = () => setCurrentPage("home");
 
     return (
         <div className={styles.doacao}>
             <Sidebar />
-            <h1 className={styles.h1}>Doe para o callme</h1>
-            <h2 className={styles.h2}>Ajude ONG's de psicologia e pessoas necessitadas! Dê um apoio para nossa plataforma:</h2>
+            <h1 className={styles.title}>Doe para o callme</h1>
+            <h2 className={styles.subtitle}>
+                Ajude ONG's de psicologia e pessoas necessitadas! Dê um apoio para nossa plataforma:
+            </h2>
 
-            <div>
-                {currentPage === "home" && ( // Tela inicial
-                    <div>
-                        <div className={styles.donationOptions}>
-                            <div className={styles.donationButtonsContainer}>
-                                <button
-                                    className={`${styles.donationButton} ${selectedButton === '5' ? styles.selected : ''}`}
-                                    onClick={() => handleButtonClick(5, '5')}>
-                                    <h3 className={styles.h3}> Doe 5 reais </h3>
-                                </button>
-                                <button
-                                    className={`${styles.donationButton} ${selectedButton === '10' ? styles.selected : ''}`}
-                                    onClick={() => handleButtonClick(10, '10')}>
-                                    <h3 className={styles.h3}> Doe 10 reais </h3>
-                                </button>
-                                <button
-                                    className={`${styles.donationButton} ${selectedButton === '15' ? styles.selected : ''}`}
-                                    onClick={() => handleButtonClick(15, '15')}>
-                                    <h3 className={styles.h3}> Doe 15 reais </h3>
-                                </button>
-                            </div>
-
-                            <div className={styles.customAmountContainer}>
-                                <div className={styles.groupInput}>
-                                    <label className={styles.customAmountLabel}>
-                                        <h4 className={styles.h4}> Adicione o valor que achar necessário:</h4>
-                                    </label>
-                                    <input
-                                        type="number"
-                                        placeholder="R$"
-                                        onChange={handleCustomAmountChange}
-                                        className={styles.customAmountInput}
-                                    />
-                                </div>
-                                <button
-                                    className={`${styles.donationButton2} ${selectedButton === '30' ? styles.selected : ''}`}
-                                    onClick={() => handleButtonClick(30, '30')}>
-                                    Doe uma cesta básica
-                                    <p> Essa doação irá para a ONG </p>
-                                </button>
-                            </div>
+            {currentPage === "home" && (
+                <div className={styles.homeContent}>
+                    <div className={styles.donationOptions}>
+                        <div className={styles.donationButtons}>
+                            <button
+                                className={`${styles.donationButton} ${selectedButton === '1' ? styles.selected : ''}`}
+                                onClick={() => handleButtonClick(1, '1')}>
+                                Doe 1 real
+                            </button>
+                            <button
+                                className={`${styles.donationButton} ${selectedButton === '5' ? styles.selected : ''}`}
+                                onClick={() => handleButtonClick(5, '5')}>
+                                Doe 5 reais
+                            </button>
+                            <button
+                                className={`${styles.donationButton} ${selectedButton === '15' ? styles.selected : ''}`}
+                                onClick={() => handleButtonClick(15, '15')}>
+                                Doe 15 reais
+                            </button>
                         </div>
 
-                        <div className={styles.progressBarContainer}>
-                            <p className={styles.selectedAmount}>Valor selecionado: R${selectedAmount.toFixed(2)}</p>
-                            <div className={styles.valor}>
-                                <div className="valorUm"> R$05,00 </div>
-                                <div className="valorDois"> R$10,00 </div>
-                                <div className="valorTres"> R$15,00 </div>
-                                <div className="valorQuatro"> R$20,00 </div>
-                            </div>
-                            <div className={styles.progressBar}>
-                                <div
-                                    className={styles.progressBarFill}
-                                    style={{ width: `${getProgressPercentage()}%` }}
-                                ></div>
-                            </div>
-                            <div className={styles.imgValor}>
-                                <img src={imgGallery.icon2 .src} alt="Icon" />
-                                <img src={imgGallery.icon1 .src} alt="Icon" />
-                                <img src={imgGallery.icon3 .src} alt="Icon" />
-                                <img src={imgGallery.icon4 .src} alt="Icon" />
-                            </div>
-                        </div>
-                        <button className={styles.pixButton} onClick={handlePixClick}>
-                            <img src={imgGallery.pix.src} alt={imgGallery.pix.src} />
-                            PIX
-                        </button>
-                    </div>
-                )}
-                {currentPage === "pix" && ( // Tela PIX
-                    <div className={styles.pixPage}>
-                        <div className={styles.qrContainer}>
-                            <img
-                                src={imgGallery.qrcode.png}
-                                alt={imgGallery.qrcode.png}
-                                className={styles.qrImage}
+                        <div className={styles.customDonation}>
+                            <label className={styles.customLabel}>Adicione o valor que achar necessário:</label>
+                            <input
+                                type="number"
+                                placeholder="R$"
+                                onChange={handleCustomAmountChange}
+                                className={styles.customInput}
                             />
-                            <div className={styles.codeBox}>
-                                <input
-                                    type="text"
-                                    value="OasdxzisiswAI890 NAA..."
-                                    readOnly
-                                    className={styles.codeInput}
-                                />
-                                <button className={styles.copyButton}>Copiar código</button>
-                            </div>
+                            <button
+                                className={`${styles.donationButton} ${selectedButton === '30' ? styles.selected : ''}`}
+                                onClick={() => handleButtonClick(30, '30')}>
+                                Doe uma cesta básica
+                                <p>Essa doação irá para a ONG</p>
+                            </button>
                         </div>
-                        <button className={styles.backButton} onClick={handleBackClick}>
-                            Voltar
-                        </button>
                     </div>
-                )}
-            </div>
-            <img src={imgGallery.macallmePag.src} alt={imgGallery.macallmePag.src} className={styles.macallmePag} />
+
+                    <div className={styles.progressBarContainer}>
+                        <div className={styles.progressBar}>
+                            <div
+                                className={styles.progressBarFill}
+                                style={{ width: `${getProgressPercentage()}%` }}
+                            ></div>
+                        </div>
+                        <p className={styles.amountLabel}>Valor selecionado: R${selectedAmount.toFixed(2)}</p>
+                        <div className={styles.iconContainer}>
+                            <img src={imgGallery.icon1.src} alt="Icon 1" />
+                            <img src={imgGallery.icon2.src} alt="Icon 2" />
+                            <img src={imgGallery.icon3.src} alt="Icon 3" />
+                            <img src={imgGallery.icon4.src} alt="Icon 4" />
+                        </div>
+                    </div>
+
+                    <button className={styles.pixButton} onClick={handlePixClick}>
+                        PIX
+                    </button>
+                </div>
+            )}
+
+            {currentPage === "pix" && (
+                <div className={styles.pixPage}>
+                    <div className={styles.qrContainer}>
+                        <img src={imgGallery.qrcode.src} alt="QR Code" />
+                        <div className={styles.codeBox}>
+                            <input
+                                type="text"
+                                value="Código Pix Exemplo"
+                                readOnly
+                                className={styles.codeInput}
+                            />
+                            <button className={styles.copyButton}>Copiar código</button>
+                        </div>
+                    </div>
+                    <button className={styles.backButton} onClick={handleBackClick}>
+                        Voltar
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
