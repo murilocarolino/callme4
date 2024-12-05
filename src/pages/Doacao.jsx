d9199a9cc1c4b7502fd37ba2fb5c9c78e8d1a578
murilocarolino <murilocsesisp@gmail.com>
2024-12-04 21:18:17 -0500

    tudo feito, falta integrar a estatística e só

--- src/pages/Doacao.jsx
+++ src/pages/Doacao.jsx
@@ -24,6 +24,7 @@ const Doacao = () => {
         return (selectedAmount / 30) * 100;
     };
 
+    const handleCartaoClick = () => setCurrentPage("cartao");
     const handlePixClick = () => setCurrentPage("pix");
     const handleBackClick = () => setCurrentPage("home");
 
@@ -32,18 +33,13 @@ const Doacao = () => {
             <Sidebar />
             <h1 className={styles.title}>Doe para o callme</h1>
             <h2 className={styles.subtitle}>
-                Ajude ONG's de psicologia e pessoas necessitadas! Dê um apoio para nossa plataforma:
+                Ajude ONG's de psicologia e pessoas necessitadas! <br /> Dê um apoio para nossa plataforma:
             </h2>
 
             {currentPage === "home" && (
                 <div className={styles.homeContent}>
                     <div className={styles.donationOptions}>
                         <div className={styles.donationButtons}>
-                            <button
-                                className={`${styles.donationButton} ${selectedButton === '1' ? styles.selected : ''}`}
-                                onClick={() => handleButtonClick(1, '1')}>
-                                Doe 1 real
-                            </button>
                             <button
                                 className={`${styles.donationButton} ${selectedButton === '5' ? styles.selected : ''}`}
                                 onClick={() => handleButtonClick(5, '5')}>
@@ -54,19 +50,26 @@ const Doacao = () => {
                                 onClick={() => handleButtonClick(15, '15')}>
                                 Doe 15 reais
                             </button>
-                        </div>
-
-                        <div className={styles.customDonation}>
-                            <label className={styles.customLabel}>Adicione o valor que achar necessário:</label>
-                            <input
-                                type="number"
-                                placeholder="R$"
-                                onChange={handleCustomAmountChange}
-                                className={styles.customInput}
-                            />
                             <button
                                 className={`${styles.donationButton} ${selectedButton === '30' ? styles.selected : ''}`}
                                 onClick={() => handleButtonClick(30, '30')}>
+                                Doe 30 reais
+                            </button>
+                        </div>
+
+                        <div className={styles.customDonation}>
+                            <div className={styles.customDiv}>
+                                <label className={styles.customLabel}>Adicione o valor que achar necessário:</label>
+                                <input
+                                    type="number"
+                                    placeholder="R$"
+                                    onChange={handleCustomAmountChange}
+                                    className={styles.customInput}
+                                />
+                            </div>
+                            <button
+                                className={`${styles.donationDoacao} ${selectedButton === '50' ? styles.selected : ''}`}
+                                onClick={() => handleButtonClick(50, '50')}>
                                 Doe uma cesta básica
                                 <p>Essa doação irá para a ONG</p>
                             </button>
@@ -92,25 +95,76 @@ const Doacao = () => {
                     <button className={styles.pixButton} onClick={handlePixClick}>
                         PIX
                     </button>
+                    <button className={styles.cartaoButton} onClick={handleCartaoClick}>
+                        Cartão
+                    </button>
                 </div>
             )}
 
             {currentPage === "pix" && (
                 <div className={styles.pixPage}>
+
                     <div className={styles.qrContainer}>
-                        <img src={imgGallery.qrcode.src} alt="QR Code" />
+                        <img src={imgGallery.qrcode.src} alt="QR Code" className={styles.qrcode} />
                         <div className={styles.codeBox}>
                             <input
                                 type="text"
-                                value="Código Pix Exemplo"
+                                value="OasdxziswIAI890 NAahajxopq00192-092nsjam"
                                 readOnly
                                 className={styles.codeInput}
                             />
                             <button className={styles.copyButton}>Copiar código</button>
                         </div>
                     </div>
+
+                    <div className={styles.progressBarContainer}>
+                        <div className={styles.progressBar}>
+                            <div
+                                className={styles.progressBarFill}
+                                style={{ width: `${getProgressPercentage()}%` }}
+                            ></div>
+                        </div>
+                        <p className={styles.amountLabel}>Valor selecionado: R${selectedAmount.toFixed(2)}</p>
+                        <div className={styles.iconContainer}>
+                            <img src={imgGallery.icon1.src} alt="Icon 1" />
+                            <img src={imgGallery.icon2.src} alt="Icon 2" />
+                            <img src={imgGallery.icon3.src} alt="Icon 3" />
+                            <img src={imgGallery.icon4.src} alt="Icon 4" />
+                        </div>
+                    </div>
                     <button className={styles.backButton} onClick={handleBackClick}>
                         Voltar
+                    </button>
+                    <button className={styles.cartaoPixButton} onClick={handleCartaoClick}>
+                        Cartão
+                    </button>
+                </div>
+            )}
+
+            {currentPage === "cartão" && (
+                <div className={styles.CartaoPage}>
+                    
+
+                    <div className={styles.progressBarContainer}>
+                        <div className={styles.progressBar}>
+                            <div
+                                className={styles.progressBarFill}
+                                style={{ width: `${getProgressPercentage()}%` }}
+                            ></div>
+                        </div>
+                        <p className={styles.amountLabel}>Valor selecionado: R${selectedAmount.toFixed(2)}</p>
+                        <div className={styles.iconContainer}>
+                            <img src={imgGallery.icon1.src} alt="Icon 1" />
+                            <img src={imgGallery.icon2.src} alt="Icon 2" />
+                            <img src={imgGallery.icon3.src} alt="Icon 3" />
+                            <img src={imgGallery.icon4.src} alt="Icon 4" />
+                        </div>
+                    </div>
+                    <button className={styles.backButton} onClick={handleBackClick}>
+                        Voltar
+                    </button>
+                    <button className={styles.cartaoPixButton} onClick={handlePixClick}>
+                        Pix
                     </button>
                 </div>
             )}
--- src/pages/Estatistica.jsx
+++ src/pages/Estatistica.jsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
+import styles from "../css/Estatisticas.module.css";
+
 const Estatistica = () => {
     return (
-        <div>
-<p>oiii</p>
+        <div className={styles.mainContent}>
+            <h3 className={styles.title}>Plano de atividades</h3>
+            <Sidebar />
+            <div className={styles.activitiesPlan}>
+                <ul className={styles.checkbox}>
+                    <li className={styles.paiCheck}>
+                        <input type="checkbox" className={styles.text} /> Aplicar e interpretar testes psicológicos.
+                    </li>
+                    <li className={styles.paiCheck}>
+                        <input type="checkbox" className={styles.text} /> Avaliações iniciais e planos de tratamento.
+                    </li>
+                    <li className={styles.paiCheck}>
+                        <input type="checkbox" className={styles.text} /> Preparar estudos de caso com base nos atendimentos realizados.
+                    </li>
+                </ul>
+            </div>
+
+            <h3 className={styles.titles}>Relatório de atividades</h3>
+            <div className={styles.activitiesReport}>
+                <button className={styles.AddAnot} >Manter um diário de observações e reflexões sobre práticas observadas
+                    <div className={styles.buttonAdd}>Adicionar anotação +</div>
+                </button>
+                <button className={styles.not} >Ver anotações anteriores</button>
+            </div>
+
         </div>
     );
+
 };
 
 export default Estatistica;
